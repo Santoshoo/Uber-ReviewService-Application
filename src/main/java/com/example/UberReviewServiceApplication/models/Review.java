2868cbd6b6cdc125e194b6ab67b31fc00eb44d7f
Santosh <sahosantosh117@gmail.com>
2025-10-28 22:27:43 +0530

    Add some more jpa functionality

--- src/main/java/com/example/UberReviewServiceApplication/models/Review.java
+++ src/main/java/com/example/UberReviewServiceApplication/models/Review.java
@@ -1,11 +1,47 @@
 package com.example.UberReviewServiceApplication.models;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 
+import javax.xml.crypto.Data;
+import java.util.Date;
+
+
+
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name="bookingreview")
 public class Review {
     @Id
-    Long id;
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;//this annotation makes id property a primary key of a table
+
+    @Column(nullable = false)
+    private String content;
+
+    private  Double rating;
+
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createdAt;
+
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedBy
+    private Date updatedAt;
+
+
+
+
 }
